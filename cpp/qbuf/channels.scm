24225a8181b45b9a2efc07a3abe751e4170ff766
David Allen <david.allen@columnzero.com>
2025-10-23 17:09:29 -0400

    Add channels file to pin versions

---
+++ cpp/qbuf/channels.scm
@@ -0,0 +1,22 @@
+(list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix)
+        (url "https://git.guix.gnu.org/guix.git")
+        (branch "master")
+        (commit
+          "bdbcdd195e4596ddef3e63979b53dff395eabbd2")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
